993bf1d3a935d2ca0f7003e170635f1b76c10e8a
GOTTIPALLIRAJESH116 <rajeshgottipalli1122002@gmail.com>
2022-11-24 12:43:33 +0530

    24-11-2022

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const port=4222;
+const bodyParser=require("body-parser")
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+app.use(bodyParser.json())
+app.use(cors())
+app.get("/",(req,res)=>{
+	res.send("Rey evarra meerantha")
+})
+app.post("/newData",(req,res)=>{
+	//console.log(req)
+	const {name,password}=req.body
+	console.log(name,password,"values")
+	res.send("Data Added")
+})
+app.listen(port,()=>{
+	console.log("Hi,This is Rajesh")
+})
